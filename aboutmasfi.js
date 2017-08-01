f964ffe70241252362febe5cb9e4a3d8980d5845
masfi-khan <khanmasfi2000@gmail.com>
2017-08-01 10:09:38 -0400

    Create aboutmasfi.js

---
+++ aboutmasfi.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Alert</h2>
+
+<button onclick="welcome()">Hello visitor</button>
+
+<script>
+function welcome() {
+    alert("Welcome to Masfi Khan's Portfolio!");
+}
+</script>
+
+
+
+</body>
+</html>
